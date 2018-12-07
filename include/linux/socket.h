331d3642fc8966f596e81c3540f9688d1a923b4f
John Fastabend <john.fastabend@gmail.com>
2018-12-07 17:42:21 +0000

    bpf: redirect to ulp

--- include/linux/socket.h
+++ include/linux/socket.h
@@ -290,6 +290,7 @@ struct ucred {
 #define MSG_BATCH	0x40000 /* sendmmsg(): more messages coming */
 #define MSG_EOF         MSG_FIN
 #define MSG_NO_SHARED_FRAGS 0x80000 /* sendpage() internal : page frags are not shared */
+#define MSG_SENDPAGE_NOPOLICY 0x10000 /* sendpage() internal : do no apply policy */
 
 #define MSG_ZEROCOPY	0x4000000	/* Use user data in kernel path */
 #define MSG_FASTOPEN	0x20000000	/* Send data in TCP SYN */
