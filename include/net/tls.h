3ee3a9ac5578907312dac5ad9cc089df55fbbe3f
John Fastabend <john.fastabend@gmail.com>
2019-06-26 16:11:14 -0700

    tls: remove close callback sock unlock/lock and flush_sync
    The tls close() callback currently drops the sock lock, makes a
    cancel_delayed_work_sync() call, and then relocks the sock. The
    same pattern is used again to call strp_done().
    
    By restructuring the code we can avoid droping lock and then
    reclaiming it. To simplify this we do the following,
    
     tls_sk_proto_close
      set_bit(CLOSING)
      set_bit(SCHEDULE)
      cancel_delay_work_sync() <- cancel workqueue
      lock_sock(sk)
      ...
      release_sock(sk)
      strp_done()
    
    Setting the CLOSING bit prevents the SCHEDULE bit from being
    cleared by any workqueue items e.g. if one happens to be
    scheduled and run between when we set SCHEDULE bit and cancel
    work. Then because SCHEDULE bit is set now no new work will
    be scheduled.
    
    Then strp_done() is called after the sk work is completed.
    Any outstanding work is sync'd and finally ctx is free'd.
    
    Tested with net selftests and bpf selftests.
    
    Signed-off-by: John Fastabend <john.fastabend@gmail.com>

--- include/net/tls.h
+++ include/net/tls.h
@@ -105,9 +105,7 @@ struct tls_device {
 enum {
 	TLS_BASE,
 	TLS_SW,
-#ifdef CONFIG_TLS_DEVICE
 	TLS_HW,
-#endif
 	TLS_HW_RECORD,
 	TLS_NUM_CONFIG,
 };
@@ -160,6 +158,7 @@ struct tls_sw_context_tx {
 	int async_capable;
 
 #define BIT_TX_SCHEDULED	0
+#define BIT_TX_CLOSING		1
 	unsigned long tx_bitmask;
 };
 
@@ -327,6 +326,7 @@ void tls_sw_close(struct sock *sk, long timeout);
 void tls_sw_free_resources_tx(struct sock *sk);
 void tls_sw_free_resources_rx(struct sock *sk);
 void tls_sw_release_resources_rx(struct sock *sk);
+void tls_sw_release_strp_rx(struct tls_context *tls_ctx);
 int tls_sw_recvmsg(struct sock *sk, struct msghdr *msg, size_t len,
 		   int nonblock, int flags, int *addr_len);
 bool tls_sw_stream_read(const struct sock *sk);
