7d71d00d1d53ac5d0f3e3a54653101436c2a60cb
Aditi Ghag <aditi@isovalent.com>
2022-11-17 00:47:45 +0000

    bpf/selftests: Tests for sock destroy BPF helper
    There are currently two tests: 1) iterating sockmap (active)
    2) iterating sockets
    
    Both the tests are working.
    
    Signed-off-by: Aditi Ghag <aditi@cilium.io>

---
+++ tools/testing/selftests/bpf/prog_tests/sock_destroy.c
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <test_progs.h>
+
+#include "sock_destroy_prog.skel.h"
+#include "network_helpers.h"
+
+static int duration;
+
+// Test using tcp sockets iterator
+//static void start_iter_tcp6(struct bpf_program *prog)
+//{
+//	struct bpf_link *link;
+//	char buf[16] = {};
+//	int iter_fd, len;
+//
+//	link = bpf_program__attach_iter(prog, NULL);
+//	if (!ASSERT_OK_PTR(link, "attach_iter"))
+//		return;
+//
+//	iter_fd = bpf_iter_create(bpf_link__fd(link));
+//	if (!ASSERT_GE(iter_fd, 0, "create_iter"))
+//		goto free_link;
+//
+//	while ((len = read(iter_fd, buf, sizeof(buf))) > 0)
+//		;
+//	CHECK(len < 0, "read", "read failed: %s\n", strerror(errno));
+//
+//	close(iter_fd);
+//
+//free_link:
+//	bpf_link__destroy(link);
+//}
+
+static void start_iter_sockmap(struct bpf_program *prog, struct bpf_map *map)
+{
+	DECLARE_LIBBPF_OPTS(bpf_iter_attach_opts, opts);
+	struct bpf_link *link;
+	char buf[16] = {};
+	int iter_fd, len;
+	union bpf_iter_link_info linfo = {};
+
+	linfo.map.map_fd = bpf_map__fd(map);
+	opts.link_info = &linfo;
+	opts.link_info_len = sizeof(linfo);
+	link = bpf_program__attach_iter(prog, &opts);
+	if (!ASSERT_OK_PTR(link, "attach_iter"))
+		return;
+
+	iter_fd = bpf_iter_create(bpf_link__fd(link));
+	if (!ASSERT_GE(iter_fd, 0, "create_iter"))
+		goto free_link;
+
+	while ((len = read(iter_fd, buf, sizeof(buf))) > 0)
+		;
+	CHECK(len < 0, "read", "read failed: %s\n", strerror(errno));
+
+	close(iter_fd);
+
+free_link:
+	bpf_link__destroy(link);
+}
+
+void test_tcp(struct sock_destroy_prog *skel)
+{
+	int serv = -1, clien = -1, n = 0;
+
+	serv = start_server(AF_INET6, SOCK_STREAM, NULL, 0, 0);
+	if (CHECK(serv < 0, "start_server", "failed to start server\n"))
+		goto cleanup_serv;
+
+	clien = connect_to_fd(serv, 0);
+	if (CHECK(clien < 0, "connect_to_fd", "errno %d\n", errno))
+		goto cleanup_serv;
+
+	serv = accept(serv, NULL, NULL);
+	if (CHECK(serv < 0, "accept", "errno %d\n", errno))
+		goto cleanup;
+	
+	n = send(clien, "t", 1, 0);
+	if (CHECK(n < 0, "client_send", "client failed to send on socket \n"))
+		goto cleanup;
+
+	start_iter_sockmap(skel->progs.iter_sockmap, skel->maps.sockmap);
+
+	n = send(clien, "t", 1, 0);
+	if (CHECK(n > 0, "client_send", "succeded on destroyed socket \n"))
+		goto cleanup;
+
+
+cleanup:
+	close(clien);
+cleanup_serv:
+	close(serv);
+}
+
+
+void test_udp(struct sock_destroy_prog *skel)
+{
+	int serv = -1, clien = -1, n = 0;
+
+	serv = start_server(AF_INET6, SOCK_DGRAM, NULL, 0, 0);
+	if (CHECK(serv < 0, "start_server", "failed to start server\n"))
+		goto cleanup_serv;
+
+	clien = connect_to_fd(serv, 0);
+	if (CHECK(clien < 0, "connect_to_fd", "errno %d\n", errno))
+		goto cleanup_serv;
+
+	n = send(clien, "t", 1, 0);
+	if (CHECK(n < 0, "client_send", "client failed to send on socket \n"))
+		goto cleanup;
+
+	start_iter_sockmap(skel->progs.iter_sockmap, skel->maps.sockmap);
+
+	n = send(clien, "t", 1, 0);
+	if (CHECK(n > 0, "client_send", "succeded on destroyed socket \n"))
+		goto cleanup;
+
+
+cleanup:
+	close(clien);
+cleanup_serv:
+	close(serv);
+
+}
+
+void test_sock_destroy(void)
+{
+	int cgroup_fd = 0;
+	struct sock_destroy_prog *skel;
+
+	skel = sock_destroy_prog__open_and_load();
+	if (!ASSERT_OK_PTR(skel, "skel_open"))
+		return;
+
+	cgroup_fd = test__join_cgroup("/sock_destroy");
+	if (CHECK(cgroup_fd < 0, "join_cgroup", "cgroup creation failed\n"))
+		goto close_cgroup_fd;
+
+	skel->links.sock_connect = bpf_program__attach_cgroup(
+		skel->progs.sock_connect, cgroup_fd);
+	if (!ASSERT_OK_PTR(skel->links.sock_connect, "prog_attach"))
+		goto close_cgroup_fd;
+
+	test_tcp(skel);
+	test_udp(skel);
+
+
+close_cgroup_fd:
+	close(cgroup_fd);
+	sock_destroy_prog__destroy(skel);
+}
---
+++ tools/testing/selftests/bpf/progs/sock_destroy_prog.c
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include "vmlinux.h"
+
+#include <bpf/bpf_helpers.h>
+
+#define AF_INET6 10
+
+struct {
+        __uint(type, BPF_MAP_TYPE_ARRAY);
+        __uint(max_entries, 1);
+        __type(key, __u32);
+        __type(value, __u64);
+} tcp_conn_sockets SEC(".maps");
+
+struct {
+        __uint(type, BPF_MAP_TYPE_ARRAY);
+        __uint(max_entries, 1);
+        __type(key, __u32);
+        __type(value, __u64);
+} udp_conn_sockets SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_SOCKMAP);
+	__uint(max_entries, 2);
+	__type(key, __u32);
+	__type(value, __u64);
+} sockmap SEC(".maps");
+
+SEC("cgroup/connect6")
+int sock_connect(struct bpf_sock_addr *ctx)
+{
+	int key = 0;
+	__u64 sock_cookie = 0;
+	__u32 keyc = 0;
+
+	if (ctx->family != AF_INET6 || ctx->user_family != AF_INET6)
+		return 1;
+
+	sock_cookie = bpf_get_socket_cookie(ctx);
+	if (ctx->protocol == IPPROTO_TCP)
+		bpf_map_update_elem(&tcp_conn_sockets, &key, &sock_cookie, 0);
+	else if (ctx->protocol == IPPROTO_UDP)
+		bpf_map_update_elem(&udp_conn_sockets, &key, &sock_cookie, 0);
+	else
+		return 1;
+
+	if (ctx->sk) 
+		bpf_map_update_elem(&sockmap, &keyc, ctx->sk, 0);
+
+	return 1;
+}
+
+SEC("iter/sockmap")
+int iter_sockmap(struct bpf_iter__sockmap *ctx)
+{
+	struct sock *sk = ctx->sk;
+	__u32 *key = ctx->key;
+	__u64 sock_cookie = 0;
+	__u32 key_s = 0;
+	__u64 *val;
+	__u16 proto;
+
+	if (!key || !sk)
+		return 0;
+
+	proto = sk->sk_protocol;
+	if (proto == IPPROTO_TCP)
+		val = bpf_map_lookup_elem(&tcp_conn_sockets, &key_s);
+	else if (proto == IPPROTO_UDP)
+		val = bpf_map_lookup_elem(&udp_conn_sockets, &key_s);
+	else
+		return 0;
+
+	if (val == (void *) 0)
+		return 0;
+
+	sock_cookie  = bpf_get_socket_cookie(sk);
+	if (sock_cookie == *val)
+		bpf_sock_destroy(sk);
+
+	return 0;
+}
+
+SEC("iter/tcp")
+int iter_tcp6(struct bpf_iter__tcp *ctx)
+{
+	struct sock_common *sk_common = ctx->sk_common;
+	struct seq_file *seq = ctx->meta->seq;
+	__u64 sock_cookie = 0;
+	__u64 *val;
+	int key = 0;
+
+	if (sk_common == (void *) 0)
+		return 0;
+
+	if (sk_common->skc_family != AF_INET6)
+		return 0;
+
+	sock_cookie  = bpf_get_socket_cookie(sk_common);
+	val = bpf_map_lookup_elem(&tcp_conn_sockets, &key);
+
+	if (val == (void *) 0)
+		return 0;
+
+	if (sock_cookie == *val) {
+		bpf_sock_destroy(sk_common);
+	}
+	
+	return 0;
+}
+
+char _license[] SEC("license") = "GPL";
